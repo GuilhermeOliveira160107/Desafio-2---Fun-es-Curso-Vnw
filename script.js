0922e76262d9ee31ad3df1ce0c2f964824815473
GuilhermeOliveira160107 <guilhermeoa.02@gmail.com>
2023-02-17 11:14:37 -0300

    primeiro comentario

---
+++ script.js
@@ -0,0 +1,40 @@
+//1 - crie uma função que exiba uma mensagem no console
+//function PaoDeQueijo(){
+    //console.log('você recebeu um pão de queijo e de bônus um caldo de cana')
+//}
+//PaoDeQueijo()
+
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+//function BoloDeMorango(parametro){
+    //console.log(parametro)
+//}
+//BoloDeMorango('você recebeu um bolo de morango e um guaraná de 2 litros')
+
+//3 - crie uma função que receba nome, idade, e um estilo musical preferido (parâmetros) e exiba no console
+//function JoaoPeDeFeijao(meuNome, minhaIdade, meuEstilodeMusica){
+    //console.log(meuNome, minhaIdade, meuEstilodeMusica)
+//}
+//JoaoPeDeFeijao('joao pé de feijão', 16,'Jazz' )
+
+//4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+//function Joy(meuFilmeFavorito,minhaMusicaFavorita){
+    //console.log(meuFilmeFavorito,minhaMusicaFavorita)
+//}
+//Joy('Homem Aranha', 'Blinding Lights')
+//5 - crie uma função que retorne o triplo do número recebido no parâmetro da função
+//function Calculadora(numero){
+   // console.log(numero * 3)
+//}
+//Calculadora(3)
+//6 - crie uma função que  verifique se uma  variável é true ou false
+let verdadeiroFalso = true
+
+function detectordementiras(verdadeiroFalso){
+if(verdadeiroFalso == true){
+console.log("e verdadeiro")
+} else{
+console.log("e falso")
+}
+}
+detectordementiras(3>1)
+detectordementiras(1>3)
